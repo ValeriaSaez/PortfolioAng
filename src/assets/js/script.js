fd179200148e5e7c7026d7e23440f3e30f0ebd51
Valeria <saez.valeria@gmail.com>
2022-05-04 21:20:48 -0300

    angular

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,3 +4,69 @@
 };*/
 
 /*Conocimientos*/
+
+$(document).ready(function () {
+  var altura = $("nav").offset().top;
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $("nav").addClass("shrink");
+    } else {
+      $("nav").removeClass("shrink");
+      $("nav").addClass("top");
+    }
+  });
+  $("#selected").on("click", function () {
+    var position = 0;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#selected").addClass("bottom");
+  });
+
+  $("#selected").on("click", function () {
+    var position = 0;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#selected").addClass("bottom");
+  });
+
+  $("#selected").on("click", function () {
+    var position = 0;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#selected").addClass("bottom");
+  });
+
+  $("#selected").on("click", function () {
+    var position = 0;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#selected").addClass("bottom");
+  });
+
+  $("#selected").on("click", function () {
+    var position = 0;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#selected").addClass("bottom");
+  });
+});
